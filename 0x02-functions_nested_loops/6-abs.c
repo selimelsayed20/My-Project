4fdd3da61e27192c4a163d4e4577e4bee855ad8a
Selim Elsayed <saleemelsayed17@gmail.com>
2022-12-14 16:08:28 +0200

    selim

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "selim.h"
 /**
  *_abs - Check Main
  * @r: An integre input
  * Description: This function returns absolute value of a number
  * Return: Absolut value of number r
  */
-int _abs(int r)
+int main(int r)
 {
 	if (r >= 0)
 	return (r);
