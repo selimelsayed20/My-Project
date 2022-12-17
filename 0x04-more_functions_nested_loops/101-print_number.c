ee5fa66414adce05d376c2b904b38f805ca7a193
Selim Elsayed <saleemelsayed17@gmail.com>
2022-12-17 15:59:34 +0200

    selim

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "101-main.c"
+#include <stdio.h>
+/**
+* print_number - Prints a number
+* @n: The number to print
+*
+*/
+void print_number(int n)
+{
+
+	if (n >= 0)
+	{
+		printf("%d",n);
+	}
+	if (n <0)
+	{
+		printf("%d",n);
+	}
+	
+}
